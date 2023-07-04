09814d4e0dad91715a45086ad90a902411f73e63
Radu Catana <rrradufirst@gmail.com>
2023-07-04 12:50:03 +0300

    Decoration Tiles

--- Assets/Scripts/LavaBlock.cs
+++ Assets/Scripts/LavaBlock.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 public class LavaBlock : MonoBehaviour
 {
     public Action<GameObject> OnLavaStay;
+    public Action<GameObject> OnLavaExit;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (OnLavaStay != null)
         {
             OnLavaStay(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (OnLavaStay != null)
+        {
+            OnLavaExit(collision.gameObject);
         }
     }
 }
