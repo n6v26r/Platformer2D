283151ba9bde9e3a2fb648bd7e050d1ae7ab94d1
n6v26r <dedu.razvan.matei@gmail.com>
2023-07-05 15:13:56 +0300

    slimey stuff

--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -1,3 +1,4 @@
+using System
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class Slime : Walker
 {
 
+    public Action<GameObject> OnSlimeHit;
     protected float JumpCooldownTimer;
     protected bool CanJump;
     [SerializeField] protected float JumpPower = 300f;
