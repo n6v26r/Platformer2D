e097447ff9b2ae0a2184fb8d7270e71917147aeb
Radu Catana <rrradufirst@gmail.com>
2023-07-27 21:01:21 +0300

    Better Dart Shooter Animations

--- Assets/Scripts/FireDartShooter.cs
+++ Assets/Scripts/FireDartShooter.cs
@@ -10,6 +10,7 @@ public class FireDartShooter : MonoBehaviour
     public Sprite[] sprites;
     //HideInInspector
     private bool ShouldShoot = false;
+    private bool CanShoot = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,7 @@ public class FireDartShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(ShouldShoot == true)
+        if(ShouldShoot == true && CanShoot == true)
         {
             StartCoroutine(Shoot());
             ShouldShoot = false;
@@ -34,13 +35,25 @@ public class FireDartShooter : MonoBehaviour
 
     private IEnumerator Shoot()
     {
+        CanShoot = false;
         yield return new WaitForSeconds(0.1f);
         spriteRenderer.sprite = sprites[1];
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = sprites[2];
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = sprites[3];
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = sprites[4];
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = sprites[5];
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = sprites[6];
         yield return new WaitForSeconds(0.2f);
         spriteRenderer.sprite = sprites[2];
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
         spriteRenderer.sprite = sprites[1];
-        yield return new WaitForSeconds(0.15f);
+        yield return new WaitForSeconds(0.3f);
         spriteRenderer.sprite = sprites[0];
+        CanShoot = true;
     }
 }
