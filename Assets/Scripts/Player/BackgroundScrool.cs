6d51ee5c718cab936fa2f0097d70b30f309fc1ca
razv <dedu.razvan.matei@gmail.com>
2023-08-05 04:35:43 +0300

    Fixed background not working.
    Background now has its own follow script and will stay in
    front of the camera.

--- Assets/Scripts/Player/BackgroundScrool.cs
+++ Assets/Scripts/Player/BackgroundScrool.cs
@@ -2,4 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScrool : MonoBehaviour{}
+public class BackgroundScrool : MonoBehaviour{
+    public GameObject Entity;
+
+    private void Update(){
+      if(Entity==null){
+            Debug.Log("<color=yellow>[BackgroundScrool WARN]: No entity specified!</color>"); 
+            return;
+        }
+      gameObject.transform.position = new Vector2(Entity.transform.position.x, Entity.transform.position.y);
+    }
+}
