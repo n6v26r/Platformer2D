55d0b1b56d9c1edec96e5906795ad3b899d0b86d
n6v26r <dedu.razvan.matei@gmail.com>
2023-08-15 20:18:56 +0300

    Modified effect system (eff will now stack)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Timeline;
 
-public class DamageEffect{
+public class DamageEffect
+{
     public float InflictDmg;
     public float iterations;
     public float delay;
     public string type;
 
-    public DamageEffect(float InflictDmg, float iterations, float delay, string type="Custom"){
+    public DamageEffect(float InflictDmg, float iterations, float delay,
+                        string type = "Custom")
+    {
         this.InflictDmg = InflictDmg;
         this.iterations = iterations;
         this.delay = delay;
@@ -22,77 +22,124 @@ public class DamageEffect{
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float health;
-    [SerializeField] private float HitSoundCooldownTime = 0.3f;
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float HitSoundCooldownTime = 0.3f;
 
     public float SpawnProtection = 0.5f;
     public float MaxHealth = 100f;
 
-    public string[] ImuneTo;
+    public string[] ImuneTo_Serializable;
+    public Hashtable ImuneTo;
 
-    private float SpawnProtectionActive;
-    
     private SoundManger SoundManager;
+    private float SpawnProtectionActive;
     private float HitSoundCooldown;
 
-    private void Awake(){
+    private List<DamageEffect> PendingDmgEff;
+
+    private void Awake()
+    {
+        PendingDmgEff = new List<DamageEffect>();
         SoundManager = FindAnyObjectByType<SoundManger>();
     }
-    private void Update(){
-        //Cooldowns
-        if(HitSoundCooldown>0)
-            HitSoundCooldown-=Time.deltaTime;
+    private void Start() { RegenHashtable(ImuneTo_Serializable); }
+    private void Update()
+    {
+        // Cooldowns
+        if (HitSoundCooldown > 0)
+            HitSoundCooldown -= Time.deltaTime;
 
-        if(SpawnProtection>0)
+        if (SpawnProtection > 0)
             SpawnProtection -= Time.deltaTime;
-        
-        if(health<=0){ // Player died
+
+        if (health <= 0)
+        { // Player died
+            PendingDmgEff.Clear();
             StopAllCoroutines(); // Cancel all working damage effects
             gameObject.transform.position = FindObjectOfType<Spawnpoint>().transform.position; // Move player to spawnpoint
             ActivateSpawnProtection();
             ResetHealth();
-        }   
-    }
-    
-    private void ResetHealth(){
-        health = MaxHealth;
+        }
     }
 
-    public void ActivateSpawnProtection(){
+    private void ResetHealth() { health = MaxHealth; }
+
+    public void RegenHashtable(string[] Keys)
+    {
+        ImuneTo = new Hashtable();
+        for (int i = 0; i < Keys.Length; i++)
+        {
+            ImuneTo.Add(Keys[i], 100);
+        }
+    }
+
+    public void ActivateSpawnProtection()
+    {
         SpawnProtectionActive = SpawnProtection;
     }
 
-    public void InflictDamage(float damage){
-        if(SpawnProtectionActive<=0)
+    public void InflictDamage(float damage)
+    {
+        if (SpawnProtectionActive <= 0)
             health -= damage;
-        if(gameObject.tag == "Player" && HitSoundCooldown<=0){
+        if (gameObject.tag == "Player" && HitSoundCooldown <= 0)
+        {
             SoundManager?.PlaySound(SoundManager.PlayerHit);
             HitSoundCooldown = HitSoundCooldownTime;
         }
     }
 
-    public float GetHealth(){
-        return health;
-    }
+    public float GetHealth() { return health; }
 
-    public void AddHealth(float buff){
+    public void AddHealth(float buff)
+    {
         health += buff;
-        if(health>MaxHealth)
+        if (health > MaxHealth)
             ResetHealth();
     }
 
-    public void ApplyEffect(DamageEffect effect){
-        for(int i=0; i<ImuneTo.Length; i++){
-            if(ImuneTo[i]==effect.type)
+    public bool SetImunity(string effect, int val)
+    {
+        if (ImuneTo.Contains(effect))
+            ImuneTo[effect] = val;
+        else
+            return false;
+        return true;
+    }
+
+    public void ApplyEffect(DamageEffect effect)
+    {
+        // If another instance of the same effect is already present, stack them;
+        for (int i = 0; i < PendingDmgEff.Count; i++)
+        {
+            if (PendingDmgEff[i].type == effect.type)
+            { // Already has this effect
+              // Stack the 2 effects:
+                PendingDmgEff[i].InflictDmg +=Mathf.Abs(PendingDmgEff[i].InflictDmg - effect.InflictDmg);
+                PendingDmgEff[i].iterations += effect.iterations;
+                PendingDmgEff[i].delay =Mathf.Min(PendingDmgEff[i].delay, effect.delay);
                 return;
+            }
         }
+
         StartCoroutine(DmgEffect(effect));
     }
 
-    IEnumerator DmgEffect(DamageEffect effect){
-        for(int i=0; i<effect.iterations; i++){
-            InflictDamage(effect.InflictDmg);
-            yield return new WaitForSeconds(effect.delay);
+    IEnumerator DmgEffect(DamageEffect effect)
+    {
+        PendingDmgEff.Add(effect);
+        int order = PendingDmgEff.Count - 1;
+        while (PendingDmgEff[order].iterations > 0)
+        {
+            if (ImuneTo.Contains(PendingDmgEff[order].type)) // If the enity has imunity to this dmg type
+                InflictDamage(PendingDmgEff[order].InflictDmg*(100 - (int)ImuneTo[PendingDmgEff[order].type]));
+            else
+                InflictDamage(PendingDmgEff[order].InflictDmg);
+            yield return new WaitForSeconds(PendingDmgEff[order].delay);
+            PendingDmgEff[order].iterations--;
         }
+        PendingDmgEff.RemoveAt(order);
     }
 }
